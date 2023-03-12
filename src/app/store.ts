4d6f6de091c5bcd534a0142a5590abdc0575f7a0
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2023-03-12 11:49:43 +0330

    change: add ant, redux-presist, react-beautiful-dnd

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,25 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({ 
+  task: counterReducer,
+  // notes: NotesReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
