8095176cd2e36b915b8b8ed72eaefc0b266d3d2e
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2023-03-12 14:14:40 +0330

    change: add action types

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,17 @@
+export enum TaskActionType {
+    CREATE_TASK = 'create_task',
+    DELETE_TASK = 'delete_task',
+    EDIT_TASK_TITLE = 'edit_title',
+    EDIT_TASK_DESC = 'edit_desc',
+    EDIT_TASK_COLUMN = 'edit_task_column',
+    DELETE_TASKS = 'delete_tasks',
+}
+
+export enum TaskColumnActionType {
+    CREATE_COLUMN = 'create_column',
+    EDIT_COLUMN_TITLE = 'edit_column_title',
+    DELETE_COLUMN = 'delete_column',
+    EDIT_ORDER_TASKS = 'edir_order_tasks',
+    ADD_NEW_TASK = 'add_new_task',
+    DELETE_TASK_IN_COLUMN = 'delete_task_in_column',
+}
