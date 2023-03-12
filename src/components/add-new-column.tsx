0af1e3b260237077232701c47e22e2636f786f14
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2023-03-12 15:18:18 +0330

    change: handle create new column

---
+++ src/components/add-new-column.tsx
@@ -0,0 +1,29 @@
+import { useAction } from "../hooks/use-actions";
+import { useState } from "react";
+import { Button, Input } from "antd";
+const AddNewColumn: React.FC = () => {
+  const [title, setTitle] = useState("");
+
+  const { createColumn } = useAction();
+
+  const createNewColumn = () => {
+    if (title) {
+      createColumn(title);
+      setTitle("");
+    }
+  };
+
+  return (
+    <div style={{ minWidth: "200px", width: "200px" }}>
+      <label htmlFor="column-id">Add another Column</label>
+      <Input
+        id="column-id"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Button onClick={createNewColumn}>Add Column</Button>
+    </div>
+  );
+};
+
+export default AddNewColumn;
